6737e1e5234320e8023a1afad69e61040c2a4744
Handiwork <johnson112009@live.cn>
2023-09-12 21:59:40 +0800

    add jsx curly brace limit

--- esm/react.js
+++ esm/react.js
@@ -13,5 +13,9 @@ export default {
     "react/react-in-jsx-scope": "off",
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-filename-extension.md
     "react/jsx-filename-extension": ["error", { extensions: [".jsx", ".tsx"] }],
+    "react/jsx-curly-brace-presence": [
+      "error",
+      { props: "never", children: "ignore", propElementValues: "always" },
+    ],
   },
 };
